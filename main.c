f415ce6066e244f25ed47aa6e1b6646adbecc316
Antonio <10717798@polimi.it>
2023-06-02 19:50:46 +0200

    Prova su input output per tutti i casi, deciso la struttura da usare

--- main.c
+++ main.c
@@ -12,6 +12,10 @@
  * 6. non tornare indietro
  * 7. ogni stazione può contenere al massimo 512 veicoli
  *
+ * utilizzo struttura dati:
+ * todo lista collegata per le stazioni
+ * todo array dinamico per auto
+ *
  */
 int main() {
 
@@ -50,6 +54,53 @@ int main() {
 
         }
 
+        else if(strcmp(token,"demolisci-stazione") ==0 ){
+            printf("+Caso rimuovi stazione\n");
+            //ottengo la distanza stazione
+            token = strtok(NULL, separatore);
+            int distanza = atoi(token);
+            printf("Distanza: %d\n",distanza);
+        }
+        else if(strcmp(token,"aggiungi-auto") ==0 ){
+            printf("+Caso aggiungi auto\n");
+            //ottengo distanza stazione(stazione)
+            token = strtok(NULL, separatore);
+            int targa = atoi(token);
+            printf("distanza-stazione: %d\n",targa);
+            //ottengo la autonomia
+            token = strtok(NULL, separatore);
+            int autonomia = atoi(token);
+            printf("Autonomia: %d\n",autonomia);
+        }
+        else if(strcmp(token,"rottama-auto") ==0 ){
+            printf("+Caso rimuovi auto\n");
+            //ottengo la distanza-stazione
+            token = strtok(NULL, separatore);
+            int distanza = atoi(token);
+            printf("Distanza-stazione: %d\n",distanza);
+            //ottengo l'autonomia
+            token = strtok(NULL, separatore);
+            int autonomia = atoi(token);
+            printf("Autonomia: %d\n",autonomia);
+        }
+        else if(strcmp(token,"pianifica-percorso") ==0 ){
+            printf("+Caso stampa percorso\n");
+            //ottengo la stazione di partenza
+            token = strtok(NULL, separatore);
+            int stazione_partenza = atoi(token);
+            printf("Stazione di partenza: %d\n",stazione_partenza);
+            //ottengo la stazione di destinazione
+            token = strtok(NULL, separatore);
+            int stazione_destinazione = atoi(token);
+            printf("Stazione di destinazione: %d\n",stazione_destinazione);
+        }
+        else if(strcmp(token,"fine") ==0 ){
+            printf("+Caso fine\n");
+            break;
+        }
+        else{
+            printf("Caso non riconosciuto\n");
+        }
     }
 
     return 0;
